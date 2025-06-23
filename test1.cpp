802cdde155e754dbcf20f77885d3fcd0d3f44b2e
abhiraj <abhiaditya860@gmail.com>
2025-06-23 20:06:43 +0530

    fizzbuzz test

--- test1.cpp
+++ test1.cpp
@@ -11,15 +11,17 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<int> arr = {-1, 2, 3, 4, -2, 6, -8, 3};
-    int ms = INT_MIN;
-    int cs = 0;
-    for(auto x : arr) {
-        cs += x;
-        ms = max(ms, cs);
-        if(cs < 0) cs = 0;
+    int n = 15;
+    for (int i = 1; i <= 15; i++) {
+        if(i % 15 == 0) {
+            cout << "FizzBuzz" << endl;
+        } else if (i % 5 == 0) {
+            cout << "Buzz" << endl;
+        } else if (i % 3 == 0) {
+            cout << "Fizz" << endl;
+        } else {
+            cout << to_string(i) << endl;
+        }
     }
-    cout << ms << endl;
-
     return 0;
 }
