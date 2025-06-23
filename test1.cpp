b9f1b9664e2af95cfd8f5f8256996e68fa2254df
abhiraj <abhiaditya860@gmail.com>
2025-06-23 19:46:32 +0530

    busy lift

--- test1.cpp
+++ test1.cpp
@@ -2,14 +2,25 @@
 #define int long long int
 using namespace std;
 
+bool compare(pair<int, int> a, pair<int, int> b) {
+    return a.second < b.second;
+}
+
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    cin >> n;
-    if (n & 1) cout << "Odd" << endl;
-    else cout << "Even" << endl;
+    vector<pair<int, int>> inp = {{7, 9}, {0, 10}, {4, 5}, {8, 9}, {4, 10},{5, 7}};
+    sort(inp.begin(), inp.end(), compare);
+    int end = inp[0].second;
+    int cnt = 1;
+    for(int i = 1; i < inp.size(); i++) {
+        if (inp[i].first >= end) {
+            cnt++;
+            end = inp[i].second;
+        }
+    }
+    cout << cnt << endl;
     return 0;
 }
