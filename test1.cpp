84a6c4f1d57b9fbb62c6b15b78ae1f3c3d1aaeb3
abhiraj <abhiaditya860@gmail.com>
2025-06-27 23:02:23 +0530

    counting set bits

--- test1.cpp
+++ test1.cpp
@@ -2,26 +2,31 @@
 #define int long long int
 using namespace std;
 
- int fastExponentiation(int a, int n) {
-        int ans = 1;
-        while(n > 0) {
-            if(n & 1) ans = ans * a;
-            a = a * a;
-            n = n >> 1;
-        }
-        return ans;
+int countingSetBits(int n) {
+    int ans = 0;
+    while(n > 0) {
+        if(n & 1) ans++;
+        n = n >> 1;
     }
+    return ans;
+}
+
+int countingSetBitsN(int n) {
+    int ans = 0;
+    while (n > 0) {
+        n = n & (n - 1);
+        ans++;
+    }
+    return ans;
+}
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-
-    int a;
-    cin >> a;
     int n;
     cin >> n;
-    cout << fastExponentiation(a, n) << endl;
+    cout << countingSetBitsN(n) << endl;
     return 0;
 }
