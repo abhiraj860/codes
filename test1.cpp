74a7562df8533c72c4af6e894301cec56c1e64b1
abhiraj <abhiaditya860@gmail.com>
2025-06-28 19:25:48 +0530

    unique number 1

--- test1.cpp
+++ test1.cpp
@@ -2,24 +2,10 @@
 #define int long long int
 using namespace std;
 
-void printSubSet(string str, int i) {
-    string out = "";
-    int j = 0;
-    while(i > 0) {
-        if(i & 1) out += str[j];
-        i = i >> 1;
-        j++; 
-    }
-    cout << out << endl;
-    return;
-}
-
-void findingSubsets(string str) {
-    int n = str.length();
-    for(int i = 0; i <= ((1 << n) - 1); i++) {
-        printSubSet(str, i);
-    }
-    return;
+int findUnique(vector<int> & arr) {
+    int ans = 0;
+    for(auto x : arr) ans ^= x;  
+    return ans;
 }
 
 int32_t main() {
@@ -27,8 +13,7 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    string str;
-    cin >> str;
-    findingSubsets(str);
+    vector<int> arr = {1, 3, 5, 4, 3, 1, 5, 4, 10};
+    cout << findUnique(arr) << endl;
     return 0;
 }
