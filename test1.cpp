2675b93f3cbed6df946bad8929c7bdffdcb03c5e
abhiraj <abhiaditya860@gmail.com>
2025-06-30 00:56:26 +0530

    sum of three values

--- test1.cpp
+++ test1.cpp
@@ -2,28 +2,31 @@
 #define int long long int
 using namespace std;
 
-int minDiffFunc(vector<int> & arr, int minDiff) {
-    int n = arr.size();
-    for(int i = 0; i <= ((1 << n) - 1); i++) {
-        int a = 0;
-        int b = 0;
-        int pos = 0;
-        int temp = i;
-        while(pos < n) {
-            if(temp & 1) {
-                a += arr[pos];
+int printThreeSum(vector<pair<int, int>> & arr, int s, int n) {
+    sort(arr.begin(), arr.end());
+    int cont = true;
+    for(int i = 0; i < n - 2; i++) {
+        int left = i + 1;
+        int right = n - 1;
+        while(left < right) {
+            int currSum = arr[i].first;
+            currSum += arr[left].first;
+            currSum += arr[right].first;
+            if(currSum == s) {
+                cout << arr[i].second << " " << arr[left].second << " " << arr[right].second << endl;
+                cont = false;
+                break;
+            } else if (currSum < s) {
+                left++;
             } else {
-                b += arr[pos];
-            } 
-            temp = temp >> 1;
-            pos++;
+                right--;
+            }
         }
-        int current = abs(a - b);
-        minDiff = min(minDiff, current);
+        if(cont == false) break;
     }
-    return minDiff;
+    if(cont == true) return -1;
+    return 0;
 }
-
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
@@ -31,15 +34,16 @@ int32_t main() {
         freopen("output.txt", "w", stdout);
     #endif
 
-    int n;
+    int n, s;
     cin >> n;
-    vector<int> arr;
+    cin >> s;
+    vector<pair<int, int>> arr;
     for(int i = 0; i < n; i++) {
         int p;
         cin >> p;
-        arr.push_back(p);
-    }    
-    int minDiff = INT_MAX;
-    cout << minDiffFunc(arr, minDiff) << endl;
+        arr.push_back({p, i + 1});
+    }
+    int ans = printThreeSum(arr, s, n);
+    if(ans == -1) cout << "IMPOSSIBLE" << endl; 
     return 0;
 }
