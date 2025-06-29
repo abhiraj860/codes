a96a0e86a7313b2c8cbf76e25648371a95701afd
abhiraj <abhiaditya860@gmail.com>
2025-06-29 22:49:43 +0530

    apple division

--- test1.cpp
+++ test1.cpp
@@ -2,17 +2,26 @@
 #define int long long int
 using namespace std;
 
-void permute(string & str, int i, set<string> & st) {
-    if(i == str.size()) {
-        st.insert(str);
-        return;
+int minDiffFunc(vector<int> & arr, int minDiff) {
+    int n = arr.size();
+    for(int i = 0; i <= ((1 << n) - 1); i++) {
+        int a = 0;
+        int b = 0;
+        int pos = 0;
+        int temp = i;
+        while(pos < n) {
+            if(temp & 1) {
+                a += arr[pos];
+            } else {
+                b += arr[pos];
+            } 
+            temp = temp >> 1;
+            pos++;
+        }
+        int current = abs(a - b);
+        minDiff = min(minDiff, current);
     }
-    for(int j = i; j < str.size(); j++) {
-        swap(str[i], str[j]);
-        permute(str, i + 1, st);
-        swap(str[i], str[j]);
-    }   
-    return;
+    return minDiff;
 }
 
 
@@ -22,12 +31,15 @@ int32_t main() {
         freopen("output.txt", "w", stdout);
     #endif
 
-    string str;
-    cin >> str;
-    set<string> st;
-    permute(str, 0, st);
-    cout << st.size() << endl;
-    for(auto x: st) cout << x << endl;
-
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for(int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+        arr.push_back(p);
+    }    
+    int minDiff = INT_MAX;
+    cout << minDiffFunc(arr, minDiff) << endl;
     return 0;
 }
