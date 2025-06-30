aa5cd554f021b7ef218056119c2bad80683ccc7e
abhiraj <abhiaditya860@gmail.com>
2025-07-01 00:11:31 +0530

    mex grid construction

--- test1.cpp
+++ test1.cpp
@@ -2,17 +2,30 @@
 #define int long long int
 using namespace std;
 
-bool compare(int a, int b) {
-    return a > b;
-}
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<int> arr = {5, 4, 3, 2, 1};
-    sort(arr.begin(), arr.end(), compare);
-    for(auto x : arr) cout << x << endl; 
+    int n;
+    cin >> n;
+    vector<vector<int>> arr(n, vector<int>(n, 0));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            set<int> st;
+            for(int k = 0; k < j; k++) st.insert(arr[i][k]);
+            for(int k = i - 1; k >= 0; k--) st.insert(arr[k][j]);
+            int mex = 0;
+            while(st.find(mex) != st.end()) mex++;
+            arr[i][j] = mex;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
