3836173c0e69ba46ba8cb98bd88c799cba027885
abhiraj <abhiaditya860@gmail.com>
2025-06-30 20:03:51 +0530

    inbuilt sorting

--- test1.cpp
+++ test1.cpp
@@ -2,38 +2,8 @@
 #define int long long int
 using namespace std;
 
-void merge(vector<int> & arr, int s, int e) {
-    int i = s;
-    int mid = (s + e) >> 1;
-    int j = mid + 1;
-    vector<int> temp;
-    while(i <= mid && j <= e) {
-        if(arr[i] <= arr[j]) temp.push_back(arr[i]), i++;
-        else temp.push_back(arr[j]), j++;
-    }
-    while(i <= mid) {
-        temp.push_back(arr[i]);
-        i++;
-    }
-    while(j <= e) {
-        temp.push_back(arr[j]);
-        j++;
-    }
-    int k = s;
-    for(auto x : temp) {
-        arr[k] = x;
-        k++;
-    }
-    return;
-}
-
-void mergeSort(vector<int> & arr, int s, int e) {
-    if(s == e) return;
-    int mid = ((s + e) >> 1);
-    mergeSort(arr, s, mid);
-    mergeSort(arr, mid + 1, e);
-    merge(arr, s, e);
-    return;
+bool compare(int a, int b) {
+    return a > b;
 }
 
 int32_t main() {
@@ -41,9 +11,8 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<int> arr = {10, 5, 2, 0, 7, 6, 4};
-    int n = arr.size();
-    mergeSort(arr, 0, n - 1);
-    for(auto x : arr) cout << x << endl;
+    vector<int> arr = {5, 4, 3, 2, 1};
+    sort(arr.begin(), arr.end(), compare);
+    for(auto x : arr) cout << x << endl; 
     return 0;
 }
