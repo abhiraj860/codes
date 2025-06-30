3e8537ceef13a7db1b1c40dde32ae48bd7bd8f77
abhiraj <abhiaditya860@gmail.com>
2025-06-30 11:18:40 +0530

    number spiral

--- test1.cpp
+++ test1.cpp
@@ -2,10 +2,19 @@
 #define int long long int
 using namespace std;
 
-int frndPairing(int n) {
-    if(n == 1) return 1;
-    if(n == 2) return 2;
-    return frndPairing(n - 1) + (n - 1) * frndPairing(n - 2);
+int spiralNumber(int row, int col, int spiralNum) {
+    if(row == 1 && col == 1) return 1;
+    int start = (spiralNum - 1) * (spiralNum - 1) + 1;
+    int end = spiralNum * spiralNum;
+
+    if(spiralNum & 1) {
+        if(row == spiralNum) return start + (col - 1);
+        else return end - (row - 1);    
+    } else {
+        if(row == spiralNum) return end - (col - 1);
+        else return start + (row - 1);
+    }
+    return -1;
 }
 
 int32_t main() {
@@ -13,8 +22,13 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    cin >> n;
-    cout << frndPairing(n) << endl;
+    int t;
+    cin >> t;
+    while(t--) {
+        int row, col;
+        cin >> row >> col;
+        int spiralNum = max(row, col);
+        cout << spiralNumber(row, col, spiralNum) << endl;
+    }    
     return 0;
 }
