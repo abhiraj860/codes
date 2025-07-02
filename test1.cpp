0d43c52b305c009ad17b635bafea6c257bdca44b
abhiraj <abhiaditya860@gmail.com>
2025-07-02 23:41:05 +0530

    staircase search

--- test1.cpp
+++ test1.cpp
@@ -2,8 +2,15 @@
 #define int long long int
 using namespace std;
 
-bool compare(string x, string y) {
-    return x + y < y + x;
+pair<int, int> search(int row, int col, vector<vector<int>> & arr, int val) {
+    int m = row - 1;
+    int n = 0;
+    while(m >= 0 && n < col) {
+        if(arr[m][n] == val) return {m, n};
+        else if (arr[m][n] < val) n++;
+        else m--;
+    }
+    return {-1, -1};
 }
 
 int32_t main() {
@@ -11,8 +18,10 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<string> input = {"a", "ab", "aba"};
-    sort(input.begin(), input.end(), compare);
-    for(auto x : input) cout << x << "" ;
+    int row = 3;
+    int col = 3;
+    vector<vector<int>> arr {{1,4,9},{2,5,10},{6,7,11}};
+    pair<int, int> p = search(row, col, arr, 9);
+    cout << p.first  << " " << p.second << endl;
     return 0;
 }
