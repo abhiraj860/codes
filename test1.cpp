a34c2226bc072aa64ff11fcf9444137fc40ea29b
abhiraj <abhiaditya860@gmail.com>
2025-07-06 20:34:22 +0530

    quick select

--- test1.cpp
+++ test1.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 int pivot(vector<int> & arr, int s, int e) {
     int i = s - 1;
-    int pivot = arr[e];
-
-    for(int j = s; j <= (e - 1); j++) {
-        if(pivot > arr[j]) i++, swap(arr[i], arr[j]);
+    int p = arr[e];
+    for(int j = s; j <= e - 1; j++) {
+        if(arr[j] < p) i++, swap(arr[j], arr[i]);
     }
     swap(arr[i + 1], arr[e]);
-    return i+1;
+    return i + 1;
 }
 
-void quickSort(vector<int> & arr, int s, int e) {
-    if(s >= e) return;
+int quickSort (vector<int> & arr, int s, int e, int k) {
     int p = pivot(arr, s, e);
-    quickSort(arr, s, p - 1);
-    quickSort(arr, p + 1, e);
-    return;
+    if(p == k) return arr[p];
+    else if (k < p) return quickSort(arr, s, p - 1, k);
+    else return quickSort(arr, p + 1, e, k); 
+    return -1;
 }
+
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<int> arr = {10, 5, 2, 0, 7, 6, 4, -1, -2, 4, -5, 12, 109};
+    vector<int> arr = {10, 5, 2, 0, 7, 6, 4};
     int n = arr.size();
-    quickSort(arr, 0, n - 1);
-    for(auto x : arr) cout << x << " ";
+    int k = 0;
+    cout << quickSort(arr, 0, n - 1, k) << endl;
     return 0;
 }
