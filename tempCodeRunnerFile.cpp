fb9c1edd65816728e18a5ed79598fb601bb1d260
abhiraj <abhiaditya860@gmail.com>
2025-07-07 20:47:47 +0530

    maximum subarray sum

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,20 @@
-arr[
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+int32_t main() {
+    int n;
+    cin >> n;
+    int ms = INT_MIN;
+    int cs = 0;
+    cout << n << endl;
+    for(int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+        cs += p;
+        ms = max(ms, cs);
+        if(cs < 0) cs = 0;
+    }
+    cout << ms << endl;
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -2,21 +2,22 @@
 #define int long long int
 using namespace std;
 
-int countWays(int n) {
-    if(n < 0) return 0;
-    if(n == 1 || n == 0) return 1;
-    return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
-}
-
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    
     int n;
     cin >> n;
-    cout << countWays(n) << endl;
-
+    int ms = INT_MIN;
+    int cs = 0;
+    for(int i = 0; i < n; i++) {
+        int p;
+        cin >> p;
+        cs += p;
+        ms = max(ms, cs);
+        if(cs < 0) cs = 0;
+    }
+    cout << ms << endl;
     return 0;
 }
