c3bbf9e1f85d630854d99a5a7bbc649419763ef5
abhiraj <abhiaditya860@gmail.com>
2025-07-07 20:19:52 +0530

    ladders cincept

--- test.cpp
+++ test.cpp
@@ -2,31 +2,21 @@
 #define int long long int
 using namespace std;
 
-int joinRopes(vector<int> & arr, int n) {
-    int maxS = 0;
-    priority_queue<int, vector<int>, greater<int>> pq(arr.begin(), arr.end());
-    while(pq.size() > 1) {
-        int a = pq.top();
-        pq.pop();
-        int b = pq.top();
-        pq.pop();
-        int rope = a + b;
-        maxS += rope;
-        pq.push(rope);
-    }
-    return maxS;
+int countWays(int n) {
+    if(n < 0) return 0;
+    if(n == 1 || n == 0) return 1;
+    return countWays(n - 1) + countWays(n - 2) + countWays(n - 3);
 }
-
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-
-    int n = 4;
-    vector<int> arr = {4, 3, 2, 6};
-    cout << joinRopes(arr, n) << endl;
+    
+    int n;
+    cin >> n;
+    cout << countWays(n) << endl;
 
     return 0;
 }
