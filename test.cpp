65aac2922b6f282d40483cfbbd234f18121f3978
abhiraj <abhiaditya860@gmail.com>
2025-07-09 21:52:41 +0530

    basic

--- test.cpp
+++ test.cpp
@@ -7,17 +7,8 @@ int32_t main() {
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    int n;
-    cin >> n;
-    int ms = INT_MIN;
-    int cs = 0;
-    for(int i = 0; i < n; i++) {
-        int p;
-        cin >> p;
-        cs += p;
-        ms = max(ms, cs);
-        if(cs < 0) cs = 0;
-    }
-    cout << ms << endl;
+
+    
+
     return 0;
 }
--- test1.cpp
+++ test1.cpp
@@ -2,37 +2,13 @@
 #define int long long int
 using namespace std;
 
-int getCount(vector<int> & arr, int p) {
-    int cnt = 0;
-    int i = 0;
-    int j = arr.size() - 1;
-    while(i < j) {
-        int curr = 0;
-        while(curr <= p) {
-            curr += arr[j];
-            curr += arr[j];
-        }
-    }
-}
-
-
-
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
 
-    int n, p;
-    cin >> n >> p;
-    vector<int>arr;
-    for(int i = 0; i < n; i++) {
-        int a;
-        cin >> a;
-        arr.push_back(a);
-    }
-    sort(arr.begin(), arr.end());
-    int cnt = getCount(arr, p);
-    cout << cnt << endl;
+    
+
     return 0;
 }
