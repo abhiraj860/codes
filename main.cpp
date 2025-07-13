0ecd2d485bee2dd329f57b273f526d022e24a69d
abhiraj <abhiaditya860@gmail.com>
2025-07-13 16:17:14 +0530

    raab game 1

--- main.cpp
+++ main.cpp
@@ -2,69 +2,54 @@
 #define int long long int
 using namespace std;
 
-// void printNQueen(vector<vector<char>> & arr) {
-//     for(int i = 0; i < 8; i++) {
-//         for(int j = 0; j < 8; j++) {
-//             cout << arr[i][j];
-//         }
-//         cout << endl;
-//     }
-//     return;
-// }
-
-bool validPos(int row, int col, vector<vector<char>> & arr) {
-    int i = row;
-    int j = col;
-    for(int k = 0; k < i; k++) {
-        if(arr[k][col] == 'Q') return false;
-    }
-    while(i >= 0 && j>= 0) {
-        if(arr[i][j] == 'Q') return false;
-        i--;
-        j--;
-    }
-    i = row;
-    j = col;
-    while(i >= 0 && j < 8) {
-        if(arr[i][j] == 'Q') return false;
-        i--;
-        j++;
-    }
-    return true;
-}
-
-void nQueenProb(int row, int col, vector<vector<char>> & arr, int tot, int & cnt) {
-    if(row >= tot) {
-        cnt++;
+void findPair(int n, int a, int b) {
+    if(a + b > n) {
+        cout << "NO" << endl; 
         return;
     } 
-   
-    for(int col = 0; col < tot; col++) {
-        if(arr[row][col] == '.') {
-            if(validPos(row, col, arr)) {
-                arr[row][col] = 'Q';
-                nQueenProb(row + 1, 0, arr, tot, cnt);
-                arr[row][col] = '.'; 
-            }
-        }
+    if(a == 0 && b >= 1) {
+        cout << "NO" << endl;
+        return;
     }
+    if(b == 0 && a >= 1) {
+        cout << "NO" << endl;
+        return;
+    }
+    vector<int>resA;
+    vector<int> resB;
+    int ties = n - (a + b);
+    for(int i = 1; i <= ties; i++) {
+        resA.push_back(i);
+        resB.push_back(i);
+    } 
+    if(a > 0) {
+        for(int i = n; i > (n - a); i--) resA.push_back(i);
+        for(int i = ties + a; i >= ties + 1; i--) resB.push_back(i); 
+    }
+    if(b > 0) {
+        for(int i = ties + b; i >= ties + 1; i--) resA.push_back(i);
+        for(int i = n; i > ties + a; i--) resB.push_back(i);
+    }
+    cout << "YES" << endl;
+    for(auto x : resA) cout << x << " ";
+    cout << endl;
+    for(auto x : resB) cout << x << " ";
+    cout << endl; 
     return;
 }
-
 
 int32_t main() {
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
         freopen("output.txt", "w", stdout);
     #endif
-    vector<vector<char>> arr(8, vector<char>(8));
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++) {
-            cin >> arr[i][j];
-        }
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        findPair(n, a, b);
     }
-    int cnt = 0;
-    nQueenProb(0, 0, arr, 8, cnt);
-    cout << cnt << endl;
     return 0;
 }
