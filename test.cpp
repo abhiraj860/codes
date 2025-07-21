b66c568a00188ded14c1202c3858677267b8f5f6
abhiraj <abhiaditya860@gmail.com>
2025-07-21 19:38:09 +0530

    first repeating letter

--- test.cpp
+++ test.cpp
@@ -8,14 +8,15 @@ int32_t main() {
         freopen("output.txt", "w", stdout);
     #endif
 
-    vector<int> v1 = {1, 45, 54, 71, 78, 12};
-    vector<int> v2 = {1, 7, 5, 4, 6, 12};
-    map<int, bool> mp;
-    for(auto x : v1) mp[x] = true;
-    vector<int> v;
-    for(auto x: v2) {
-        if(mp[x]) v.push_back(x);
+    string input = "codingminutes";
+    map<char, bool> mp;
+    for(auto x : input) {
+        if(mp[x]) {
+            cout << x << endl;
+            break;
+        }
+        mp[x] = true;
     }
-    for(auto x : v) cout << x << endl;
+
     return 0;
 }
